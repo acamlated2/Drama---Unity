2be87db484d5f9cc14c27d81fda5204422001ad1
Calvin Oktavialdy Setia <calvin2.setia@live.uwe.ac.uk>
2023-11-04 19:14:54 +0000

    blocked off scene, return position, lure distance, bag model

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,44 @@ public class PlayerController : MonoBehaviour
 {
     private NavMeshAgent _agent;
 
+    [SerializeField] private float timerMax = 2;
+    private float _timer = 5;
+
+    private GameObject _returnPos;
+
+    [SerializeField] private float lureDistance = 10;
+
     private void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
+        
+        _returnPos = GameObject.FindGameObjectWithTag("ReturnPosition");
     }
 
     public void Lure(Vector3 pos)
     {
-        _agent.SetDestination(pos);
+        float distance = Vector3.Distance(transform.position, pos);
+        if (distance <= lureDistance)
+        {
+            _agent.SetDestination(pos);
+            _timer = timerMax;
+        }
+        
+        Debug.Log(distance);
+    }
+
+    private void Update()
+    {
+        if (_agent.remainingDistance <= _agent.stoppingDistance && _agent.remainingDistance != Mathf.Infinity)
+        {
+            _timer -= 1 * Time.deltaTime;
+
+            if (_timer <= 0)
+            {
+                _agent.SetDestination(_returnPos.transform.position);
+
+                _timer = timerMax;
+            }
+        }
     }
 }
